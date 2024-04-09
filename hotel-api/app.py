f4d3a1dec10dc462733f6e6fab9892a76fe0b5d4
Hakkinev <vinski.hakkinen@gmail.com>
2024-04-09 16:24:19 +0000

    add psycopg

--- hotel-api/app.py
+++ hotel-api/app.py
@@ -1,41 +1,55 @@
-from flask import Flask, request, jsonify
+import os, psycopg
+from psycopg.rows import dict_row
+from flask import Flask, request
 from flask_cors import CORS 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PORT=8167 # hakkinevs port
+
+db_url = os.environ.get("DB_URL")
+print(os.environ.get("ASD"))
+
+conn = psycopg.connect(db_url, autocommit=True, row_factory=dict_row)
 
 app = Flask(__name__)
 CORS(app) # Tillåt cross-origin requests
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-    #return "<h1>Hello, Flask!</h1>"
-    return {
-        'greeting': "Hello, Hotel API!", 
-        'method': request.method
-        }
+rooms = [
+    {"number": 101, "type": "Standard ", "price": 100},
+    {"number": 102, "type": "Deluxe ", "price": 200},
+    {"number": 103, "type": "Family ", "price": 150}
+]
 
-@app.route("/test", methods=['GET', 'POST'])
-def test():
+@app.route("/test", )
+def dbtest():
+    with conn.cursor() as cur:
+        cur.execute("SELECT * from people")
+        rows = cur.fetchall()
+        return rows
+
+@app.route("/", )
+def info():
+    #return "<h1>Hello, Flask!</h1>"
+    return "Hotel API, endpoints /rooms, /bookings"
+
+@app.route("/rooms", methods=['GET', 'POST'])
+def rooms_endpoint():
     if request.method == 'POST':
-        # skapa rad i databasen, returnera ny id...
-        new_id = 555
+        request_body = request.get_json()
+        print(request_body)
+        rooms.append(request_body)
         return {
-            'msg' :"Du har skapat en ny rad till databasen, id är {new_id}",
-            'method' : request.method
+            'msg' :f"Du har skapat en nytt rum, id är {len(rooms)-1}"
         }
     else:
-        return {
-            'msg' : "TESTING!",
-            'method' : request.method
-        }
+        return rooms
 
-@app.route("/test/<int:id>", methods=['GET', 'POST', 'PUT', 'DELETE' ])
-def testId(id):
+@app.route("/rooms/<int:id>", methods=['GET', 'PUT', 'PATCH', 'DELETE' ])
+def one_room_endpoint(id):
     if request.method == 'GET':
-        return {
-            'msg' :f"Din requestade id: {id} ",
-            'method' : request.method
-        }
+        return rooms[id]
     if request.method == 'PUT' or request.method == 'PATCH':
         return {
             'msg' :f"Du uppdaterar id: {id} ",
@@ -47,13 +61,6 @@ def testId(id):
             'method' : request.method
         }
 
-
-
-@app.route("/ip")
-def ip():
-    
-    visitor_ip = request.remote_addr  # Hämta besökarens IP-adress
-    return jsonify({'ip': visitor_ip})  # Returnera IP-adressen i JSON-format
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=True, ssl_context=(
